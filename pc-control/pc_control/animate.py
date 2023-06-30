fbb680b624cb011d5d94c0f9c121716f1046b89b
_MircoWave_ <clarknick1990@gmail.com>
2023-06-30 22:04:30 +0100

    Update animate.py
    Added basic point types.
    Added sections.

--- pc-control/pc_control/animate.py
+++ pc-control/pc_control/animate.py
@@ -3,26 +3,56 @@ import pygame
 point_colour = (255, 255, 255)
 
 class BasicPoint():
-    def __init__(self, left, top, display):
+    def __init__(self, display, left, top, type="lu", width=50, height=20):
         self.left = left
         self.top = top
         self.display = display
-        self.width = 50
-        self.height = 50
+        self.width = width
+        self.height = height
         self.fixed_offset = 10
-        self.rect = pygame.Rect(left, top, self.width, self.height)
-        
-        self.offset = 0
+    
+        self.hover_colour = (235, 52, 61)
+        self.moving_colour = (235, 216, 52)
+        self.pos1_colour = (55, 235, 52)
+        self.pos2_colour = (52, 140, 235)
+        self.boundary_colour = (255,255,255)
 
-        self.hover_colour = (0, 255, 0)
-        self.moving_colour = (255, 0, 0)
-        self.line_colour = (255, 255, 255)
+        self.line_colour = self.pos1_colour
 
-        self.line_start = (left, top+self.fixed_offset)
-        self.line_end = [left+self.width-1, top+self.fixed_offset]
+        self.pos1_vpos = top
+        self.pos2_vpos = top+self.height
 
-        self.pos1_vpos = top+self.fixed_offset
-        self.pos2_vpos = top+self.height - self.fixed_offset
+        match type:
+            case "lu":
+                self.increment = -1
+                self.pos2_vpos = top-self.height
+                box_top = top-self.fixed_offset-height
+                box_left = left
+                self.line_start = (left, top)
+                self.line_end = [left+self.width, top]
+            case "ld":
+                self.increment = 1
+                self.pos2_vpos = top+self.height
+                box_top = top-self.fixed_offset
+                box_left = left
+                self.line_start = (left, top)
+                self.line_end = [left+self.width, top]
+            case "ru":
+                self.increment = -1
+                self.pos2_vpos = top-self.height
+                box_top = top-self.fixed_offset-height
+                box_left = left-self.width
+                self.line_start = (left, top)
+                self.line_end = [left-self.width, top]
+            case "rd":
+                self.increment = 1
+                self.pos2_vpos = top+self.height
+                box_top = top-self.fixed_offset
+                box_left = left-self.width
+                self.line_start = (left, top)
+                self.line_end = [left-self.width, top]
+
+        self.rect = pygame.Rect(box_left, box_top, self.width+1, self.height+2*self.fixed_offset)
 
         # possible states: pos1, pos2, moving_to_pos1, moving_to_pos2
         self.state = "pos1"
@@ -30,7 +60,7 @@ class BasicPoint():
     def update_state(self, mouse_pos, mouse_up):
         if self.rect.collidepoint(mouse_pos):
             
-            if self.state == "pos1":
+            if self.state == "pos1" or self.state == "pos2":
                 self.line_colour = self.hover_colour
 
             if mouse_up:
@@ -44,26 +74,37 @@ class BasicPoint():
                         self.state = "moving_to_pos2"
                     case "moving_to_pos2":
                         self.state = "moving_to_pos1"
-        else:
-            self.line_colour =(255, 255, 255)
+        elif self.state == "pos1":
+            self.line_colour = self.pos1_colour
+        elif self.state == "pos2":
+            self.line_colour = self.pos2_colour
+
 
     def draw(self):
         #pygame.draw.rect(self.display, (255, 0, 0), self.rect)
 
         match self.state:
             case "moving_to_pos1":
-                self.line_end[1] -= 1
+                self.line_end[1] -= self.increment
                 if self.line_end[1] == self.pos1_vpos:
                     self.state = "pos1"
-                    self.line_colour = (255, 255, 255)
             case "moving_to_pos2":
-                self.line_end[1] += 1 
+                self.line_end[1] += self.increment
                 if self.line_end[1] == self.pos2_vpos:
                     self.state = "pos2"
-                    self.line_colour = (255, 255, 255)
 
         pygame.draw.line(self.display, self.line_colour, self.line_start, self.line_end, 5)
+        pygame.draw.rect(self.display, self.boundary_colour, self.rect, 1)
 
+class Section():
+    def __init__(self, display, start, end):
+        self.display = display
+        self.start = start
+        self.end = end
+        self.line_colour = (255,255,255)
+    
+    def draw(self):
+        pygame.draw.line(self.display, self.line_colour, self.start, self.end, 5)
 
 
 pygame.init()
@@ -82,7 +123,13 @@ animation_speed = 2
 animation_direction = 1
 
 
-points = [BasicPoint(100,100,screen)]
+points = [BasicPoint(screen, 100,100, type="ru"), 
+          BasicPoint(screen, 100,200, type="rd"),
+          BasicPoint(screen, 300,100, type="lu"),
+          BasicPoint(screen, 300,200, type="ld")]
+
+sections = [Section(screen, (100, 100), (300,100)),
+        Section(screen, (100, 200), (300,200))]
 
 # Start the game loop
 running = True
@@ -101,28 +148,15 @@ while running:
         point.update_state(mouse_pos, mouse_up)
     
     mouse_up = False
-    # Create a Rect object at position (100, 100) with width 50 and height 50
-
-    # Draw a red rectangle using the Rect object
-
-
-    # Update the line position
-    line_start = (line_start[0] + animation_speed * animation_direction, line_start[1])
-    line_end = (line_end[0] + animation_speed * animation_direction, line_end[1])
-
-    
-
-    # Reverse direction if we hit an edge
-    if line_start[0] < 0 or line_end[0] > screen.get_width():
-        animation_direction *= -1
 
     # Draw the line
     screen.fill((0, 0, 0))
-    pygame.draw.line(screen, line_color, line_start, line_end, line_width)
-
 
     for point in points:
         point.draw()
+
+    for section in sections:
+        section.draw()
 
     pygame.display.flip()
 
