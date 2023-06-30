aeb553dbbce9592028795fcf4f65b8de7b094440
_MircoWave_ <clarknick1990@gmail.com>
2023-06-30 22:07:17 +0100

    Refactored into new filenames

--- pc-control/pc_control/main.py
+++ pc-control/pc_control/main.py
@@ -1,34 +1,73 @@
 import pygame
+from pc_control import points
+
+point_colour = (255, 255, 255)
+
+
+class Section():
+    def __init__(self, display, start, end):
+        self.display = display
+        self.start = start
+        self.end = end
+        self.line_colour = (255,255,255)
+    
+    def draw(self):
+        pygame.draw.line(self.display, self.line_colour, self.start, self.end, 5)
+
 
 pygame.init()
 
-# Set up the drawing window
-screen = pygame.display.set_mode([500, 500])
+# Set up the display
+screen = pygame.display.set_mode((400, 400))
 
-# Set up the initial line position
-line_start = (0, 0)
-line_end = (250, 250)
+# Set up the line
+line_color = (255, 255, 255)
+line_start = (50, 200)
+line_end = (150, 250)
+line_width = 5
 
-# Draw an anti-aliased line
-pygame.draw.aaline(screen, (255, 255, 255), line_start, line_end)
+# Set up the animation
+animation_speed = 2
+animation_direction = 1
 
-# Update the screen
-pygame.display.flip()
 
-# Run until the user asks to quit
+points = [points.BasicPoint(screen, 100,100, type="ru"), 
+          points.BasicPoint(screen, 100,200, type="rd"),
+          points.BasicPoint(screen, 300,100, type="lu"),
+          points.BasicPoint(screen, 300,200, type="ld")]
+
+sections = [Section(screen, (100, 100), (300,100)),
+        Section(screen, (100, 200), (300,200))]
+
+# Start the game loop
 running = True
+mouse_up = False
 while running:
+    # Handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            # Move the line to the mouse position
-            line_start = event.pos
-            line_end = (line_end[0] + event.pos[0], line_end[1] + event.pos[1])
-            # Redraw the line
-            screen.fill((0, 0, 0))
-            pygame.draw.aaline(screen, (255, 255, 255), line_start, line_end)
-            pygame.display.flip()
+        elif event.type == pygame.MOUSEBUTTONUP:
+            mouse_up = True
 
-# Quit pygame properly
+    mouse_pos = pygame.mouse.get_pos()
+
+    for point in points:
+        point.update_state(mouse_pos, mouse_up)
+    
+    mouse_up = False
+
+    # Draw the line
+    screen.fill((0, 0, 0))
+
+    for point in points:
+        point.draw()
+
+    for section in sections:
+        section.draw()
+
+    pygame.display.flip()
+
+    pygame.time.wait(10)
+
 pygame.quit()
--- pc-control/pc_control/points.py
+++ pc-control/pc_control/points.py
@@ -1,6 +1,5 @@
 import pygame
 
-point_colour = (255, 255, 255)
 
 class BasicPoint():
     def __init__(self, display, left, top, type="lu", width=50, height=20):
@@ -95,71 +94,3 @@ class BasicPoint():
 
         pygame.draw.line(self.display, self.line_colour, self.line_start, self.line_end, 5)
         pygame.draw.rect(self.display, self.boundary_colour, self.rect, 1)
-
-class Section():
-    def __init__(self, display, start, end):
-        self.display = display
-        self.start = start
-        self.end = end
-        self.line_colour = (255,255,255)
-    
-    def draw(self):
-        pygame.draw.line(self.display, self.line_colour, self.start, self.end, 5)
-
-
-pygame.init()
-
-# Set up the display
-screen = pygame.display.set_mode((400, 400))
-
-# Set up the line
-line_color = (255, 255, 255)
-line_start = (50, 200)
-line_end = (150, 250)
-line_width = 5
-
-# Set up the animation
-animation_speed = 2
-animation_direction = 1
-
-
-points = [BasicPoint(screen, 100,100, type="ru"), 
-          BasicPoint(screen, 100,200, type="rd"),
-          BasicPoint(screen, 300,100, type="lu"),
-          BasicPoint(screen, 300,200, type="ld")]
-
-sections = [Section(screen, (100, 100), (300,100)),
-        Section(screen, (100, 200), (300,200))]
-
-# Start the game loop
-running = True
-mouse_up = False
-while running:
-    # Handle events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.MOUSEBUTTONUP:
-            mouse_up = True
-
-    mouse_pos = pygame.mouse.get_pos()
-
-    for point in points:
-        point.update_state(mouse_pos, mouse_up)
-    
-    mouse_up = False
-
-    # Draw the line
-    screen.fill((0, 0, 0))
-
-    for point in points:
-        point.draw()
-
-    for section in sections:
-        section.draw()
-
-    pygame.display.flip()
-
-    pygame.time.wait(10)
-
-pygame.quit()
