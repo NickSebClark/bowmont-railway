75c6f40ddeed50bfbe30b7f98074d74eb5432a54
_MircoWave_ <clarknick1990@gmail.com>
2023-07-11 21:53:21 +0100

    Initial tries with serial comms.

--- pc-control/pc_control/serial_comms.py
+++ pc-control/pc_control/serial_comms.py
@@ -1,4 +1,6 @@
 import tomllib
+import serial
+from time import sleep
 
 def connect():
     with open("settings.toml", "rb") as f:
@@ -7,6 +9,19 @@ def connect():
     print(settings['port'])
     print(settings['baud'])
 
+    ser = serial.Serial(settings['port'], settings['baud'], timeout=0.1)
+
+    while True:
+
+        data = ser.readlines()
+
+        for line in data:
+            try:
+                print(line.decode('ascii'))
+            except:
+                print('decode fail')
+
+        sleep(0.1)
 
 if __name__ == "__main__":
     connect()
