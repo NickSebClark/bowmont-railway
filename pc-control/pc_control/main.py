7de9fc32a3dd4f7ce7ed91f7f527daf1969a6e46
_MircoWave_ <clarknick1990@gmail.com>
2023-07-27 22:16:20 +0100

    Added request sync button

--- pc-control/pc_control/main.py
+++ pc-control/pc_control/main.py
@@ -5,6 +5,9 @@ import pygame.freetype
 from pc_control.layout import Layout
 from pc_control.serial_comms import ZeroWaitSerial, read_connection_settings
 from datetime import datetime
+from pygame_widgets.button import Button
+import pygame_widgets
+
 
 def main():
     """Basic pygame setup and main event loop."""
@@ -21,14 +24,14 @@ def main():
     title_font = pygame.font.Font("pc-control/resources/britrdn_.ttf", 39)
     title_surface = title_font.render("Bowmont Town", True, (255, 255, 255))
     sign_outline = pygame.Rect(width / 2 - title_surface.get_width() / 2 - 10, 5, title_surface.get_width() + 20, 40)
-    
-    #Serial Monitor Text
+
+    # Serial Monitor Text
     monitor_font = pygame.font.SysFont("Consolas", 12)
-    serial_monitor_buffer = ['']*5
+    serial_monitor_buffer = [""] * 5
 
     # Set up the display
     screen = pygame.display.set_mode((width, height))
-    
+
     ser = ZeroWaitSerial(port, baud)
 
     layout = Layout(ser)
@@ -37,15 +40,31 @@ def main():
     image = pygame.image.load("pc-control/resources/sign_small.png")
     roundel = pygame.image.load("pc-control/resources/roundel.png")
 
-    
+    sync_button = Button(
+        screen,
+        width - 55,
+        height - 35,
+        50,
+        30,
+        fontSize=15,
+        text="SYNC",
+        inactiveColour=(200, 50, 0),
+        hoverColour=(150, 0, 0),
+        pressedColour=(0, 200, 20),
+        textColor=(100, 100, 100),
+        radius=8,
+        onClick = lambda: request_sync(ser)
+    )
 
     running = True
 
     while running:
         mouse_up = False
 
+        events = pygame.event.get()
+
         # Handle events
-        for event in pygame.event.get():
+        for event in events:
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.MOUSEBUTTONUP:
@@ -54,6 +73,8 @@ def main():
         mouse_pos = pygame.mouse.get_pos()
 
         screen.fill((0, 0, 0))
+
+        pygame_widgets.update(events)
 
         # Draw the layout with the mouse position relative to the layout pos.
         layout.draw((mouse_pos[0] - layout_pos[0], mouse_pos[1] - layout_pos[1]), mouse_up)
@@ -65,7 +86,6 @@ def main():
         screen.blit(roundel, (width - 40 - 5, 5))
         pygame.draw.rect(screen, (255, 255, 255), sign_outline, 2)
 
-
         lines = ser.read_available_lines()
         process_lines(lines, layout, serial_monitor_buffer)
         draw_serial_monitor(monitor_font, serial_monitor_buffer, height, screen)
@@ -76,35 +96,43 @@ def main():
 
     pygame.quit()
 
+def request_sync(ser:ZeroWaitSerial):
+    print("request sync")
+    ser.write(str.encode("r\n"))
+
+
 def draw_serial_monitor(font, buffer, top, screen):
     for index, line in enumerate(buffer):
-        #draw to the screen
-        rendered_line = font.render(line, True, (255, 255, 255))
-        v_pos = top-12*(len(buffer)-index)
-        screen.blit(rendered_line, (5, v_pos))
+        # draw to the screen
+        try:
+            rendered_line = font.render(line, True, (255, 255, 255))
+            v_pos = top - 12 * (len(buffer) - index)
+            screen.blit(rendered_line, (5, v_pos))
+        except ValueError:
+            print("Unable to display " + line)
 
 
-def process_lines(new_lines:list[str], layout:Layout, monitor_buffer:list[str]):
-
+def process_lines(new_lines: list[str], layout: Layout, monitor_buffer: list[str]):
     for line in new_lines:
         monitor_buffer.append(f"{datetime.now().strftime('%H:%M:%S')}: {line}")
         monitor_buffer.pop(0)
 
         match line[0]:
-            case 'S':
-                if line[1] == '0' or line[1] == '1':
+            case "S":
+                if line[1] == "0" or line[1] == "1":
                     try:
                         states = [int(char) for char in line[1:].strip()]
-                        states[7] = int(line[7:9], 2) #convert the sidings into a single number
-                        #remove the doubles
+                        states[7] = int(line[7:9], 2)  # convert the sidings into a single number
+                        # remove the doubles
                         states.pop(8)
                         states.pop(6)
                         states.pop(3)
                         layout.update_points(states)
                     except Exception as e:
                         print(e)
-            case '<':
+            case "<":
                 pass
+
 
 if __name__ == "__main__":
     main()
